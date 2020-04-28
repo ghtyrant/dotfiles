adc3134036b7dc8d4641cbe34a060724328ca4fa
Fabian Schlager <fabian.schlager@skyr.at>
2020-04-28 12:36:42 +0200

    Add secondary keyboard layout, move all apps to startup.sh

--- files/config/i3/startup.sh
+++ files/config/i3/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+seafile-applet &
+xscreensaver &
 setxkbmap de &
 feh --bg-fill ~/docs/Seafile/Main/wallpaper/firewatch_bluesky.png &
 feh-blur --darken 0 -b 10 -d &
+setxkbmap -layout de,us -model pc105 -variant nodeadkeys,altgr-intl -option grp:alt_shift_toggle

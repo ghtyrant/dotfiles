718fd68ae0b8313410632ab9a3270f03e8a65950
Fabian Schlager <fabian.schlager@skyr.at>
2020-04-28 12:25:22 +0200

    Use .png of wallpaper, tweak blur settings

--- files/config/i3/startup.sh
+++ files/config/i3/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 setxkbmap de &
-feh --bg-fill ~/docs/Seafile/Main/wallpaper/firewatch_bluesky.jpg &
-feh-blur --darken 20 -b 20 -d &
+feh --bg-fill ~/docs/Seafile/Main/wallpaper/firewatch_bluesky.png &
+feh-blur --darken 0 -b 10 -d &

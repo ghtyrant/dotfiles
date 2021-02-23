1a50fc36469037b875f0b18530b5f4357c144a1a
Fabian Schlager <fabian.schlager@skyr.at>
2021-02-23 09:23:24 +0100

    Hard code use of card with index

--- files/scripts/pachprofile.sh
+++ files/scripts/pachprofile.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
 CARD_ID=1
 PROFILE_HEADSET="output:analog-stereo+input:analog-stereo"
 PROFILE_MONITOR="output:iec958-stereo+input:analog-stereo"
 
-CURRENT_PROFILE=$(pacmd list-cards | grep "active profile" | tail -n 1 | awk '{split($3, arr, "[<>]"); print arr[2]}')
+CURRENT_PROFILE=$(pacmd list-cards | grep "active profile" | sed -n 2p | awk '{split($3, arr, "[<>]"); print arr[2]}')
 
 if [[ "$CURRENT_PROFILE" == "$PROFILE_MONITOR" ]]; then
   pacmd set-card-profile $CARD_ID $PROFILE_HEADSET
